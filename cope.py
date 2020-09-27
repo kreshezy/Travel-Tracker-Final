a263c4a0ce3bb7f83a7661f94140a3c47852414a
kreshezy <68359935+kreshezy@users.noreply.github.com>
2020-09-27 10:02:42 +0800

    Delete cope.py

--- cope.py
+++
@@ -1,9 +0,0 @@
-while True:
-    age = int(input("Please enter your age: "))
-    if age < 0 or age > 130:
-        print("That age is invalid")
-    else:
-        print("Age accepted. Have a nice day")
-        break
-
-
